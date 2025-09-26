78a633f10c530ada08bf9f204289c728bed29b9e
dena416 <w70996300@gmail.com>
2025-09-26 16:03:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CMoE1QgY#345GaG9ZA9TXvjJxYv726fUCGzHwNnAZk5mDtxliA1Y", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "PIAVVBgI#eEUt8xrhQ51kR2f82WBEXCUxXXGvSSFtkKynC_A-Bko", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94720630290",
